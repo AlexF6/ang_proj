dbab5172af7b93df527912ee3e0f717b2d56c209
AlexF6 <perol2894@gmail.com>
2025-09-02 10:41:42 -0500

    feat: add Angular Icons

--- src/app/shared/ui/navbar.ts
+++ src/app/shared/ui/navbar.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import { aspectsUser } from '@ng-icons/ux-aspects';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [NgIcon],
   templateUrl: './navbar.html',
+  viewProviders: [provideIcons({ aspectsUser })]
 })
 export class NavBar {
 
