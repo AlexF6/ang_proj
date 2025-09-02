ee3a6a06686bb8c57edd65aadbf9909892e542a0
AlexsP25 <agonzalezf@curatechsas.com>
2025-09-02 10:25:47 -0500

    feat: start project structure

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HelloWorld } from './hello-world/hello-world';
+import { NavBar } from './shared/ui/navbar'
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HelloWorld],
+  imports: [RouterOutlet, HelloWorld, NavBar],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
